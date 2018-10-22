36e032a9f3888aa7feb55c68690b9a630d246772
Johnny Kekker <giomail.iv@gmail.com>
2018-10-22 20:28:22 +0300

    navigation bean in da house, input y value, values validation

--- src/main/scala/b4/GraphFormBean.scala
+++ src/main/scala/b4/GraphFormBean.scala
@@ -2,6 +2,10 @@ package b4
 
 import scala.beans.BeanProperty
 import javax.faces.bean.{ManagedBean, SessionScoped, ManagedProperty}
+import javax.faces.application.FacesMessage
+import javax.faces.validator.ValidatorException
+import javax.faces.context.FacesContext
+import javax.faces.component.UIComponent
 import b4.SessionDataBean
 import b4.History
 
@@ -13,6 +17,24 @@ class GraphFormBean extends Serializable {
   @BeanProperty var y: Double = 0.0
   @ManagedProperty(value="#{sessionData}")
   @BeanProperty var sessionData = new SessionDataBean()
+
+  def validateX(ctx: FacesContext, component: UIComponent, v: Object) {
+    if (v.asInstanceOf[Double] < -5.0 || v.asInstanceOf[Double] > 3.0) {
+      throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "x shold be in [-5; 3]", ""))
+    }
+  }
+
+  def validateY(ctx: FacesContext, component: UIComponent, v: Object) {
+    if (v.asInstanceOf[Double] < -5.0 || v.asInstanceOf[Double] > 3.0) {
+      throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "y shold be in [-5; 3]", ""))
+    }
+  }
+
+  def validateR(ctx: FacesContext, component: UIComponent, v: Object) {
+    if (v.asInstanceOf[Double] < 2.0 || v.asInstanceOf[Double] > 5.0) {
+      throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "r shold be in [2; 5]", ""))
+    }
+  }
 
   def compute() {
   	val history = new History()
---
+++ src/main/scala/b4/NavigationBean.scala
@@ -0,0 +1,11 @@
+package b4
+
+import scala.beans.BeanProperty
+import javax.faces.bean.{ManagedBean, RequestScoped}
+import java.util.ArrayList
+
+@ManagedBean(name = "navigation")
+@RequestScoped
+class NavigationBean extends Serializable {
+  def history(): String = "history"
+}
