1862726b0af343e57b2d43236a71aa62a5a913fa
timlathy <tim.labushev@gmail.com>
2018-10-23 16:25:05 +0300

    port vue.js page layout to jsf

--- src/main/scala/b4/GraphFormBean.scala
+++ src/main/scala/b4/GraphFormBean.scala
@@ -15,6 +15,37 @@ class GraphFormBean extends Serializable {
   @BeanProperty var r: Double = 0.0
   @BeanProperty var x: Double = 0.0
   @BeanProperty var y: Double = 0.0
+  @BeanProperty var graph: String = """
+  <svg class="graph" ref="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+    <g id="graph__coordinate-plane">
+      <!-- horizontal and vertical axes -->
+      <path fill="none" stroke="#000" stroke-width="1px" d="M0 199h400"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M199 0v400"/>
+      <!-- arrows -->
+      <path fill="none" stroke="#000" stroke-width="1px" d="M199 0l-3 7"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M199 0l3 7"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M400 199l-6-3"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M400 199l-6 3"/>
+    </g>
+    <path fill="none" stroke="#000" stroke-width="1px" d="M199 39l160 160"/>
+    <path fill="none" stroke="#000" stroke-width="1px" d="M359 199v80"/>
+    <path fill="none" stroke="#000" stroke-width="1px" d="M359 279H199"/>
+    <path fill="none" stroke="#000" stroke-width="1px" d="M199 279q-80-10-80-80"/>           
+    <text x="180" y="44" font-weight="400">
+      <tspan x="180" y="44" font-size="16px">R</tspan>
+    </text>
+    <text x="106" y="190" font-weight="400">
+      <tspan x="106" y="190" font-size="16px">R/2</tspan>
+    </text>
+    <text x="166" y="296" font-weight="400">
+      <tspan x="166" y="296" font-size="16px">R/2</tspan>
+    </text>
+    <text x="360" y="190" font-weight="400">
+      <tspan x="360" y="190" font-size="16px">R</tspan>
+    </text>
+  </svg>
+  """
+
   @ManagedProperty(value="#{sessionData}")
   @BeanProperty var sessionData = new SessionDataBean()
 
