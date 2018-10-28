ffb0ad6d75286334aebf41d0620dfc82a00310e3
Johnny Kekker <giomail.iv@gmail.com>
2018-10-28 21:49:29 +0300

    new graph

--- src/main/scala/b4/GraphBean.scala
+++ src/main/scala/b4/GraphBean.scala
@@ -30,17 +30,18 @@ class GraphBean extends Serializable {
         <path fill="none" stroke="#000" stroke-width="1px" d="M400 199l-6-3"/>
         <path fill="none" stroke="#000" stroke-width="1px" d="M400 199l-6 3"/>
       </g>
-      <path fill="none" stroke="#000" stroke-width="1px" d="M199 39l160 160"/>
-      <path fill="none" stroke="#000" stroke-width="1px" d="M359 199v80"/>
-      <path fill="none" stroke="#000" stroke-width="1px" d="M359 279H199"/>
-      <path fill="none" stroke="#000" stroke-width="1px" d="M199 279q-80-10-80-80"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M39 199l160 160"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M39 119v80"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M39 119H199"/>
+      <path fill="none" stroke="#000" stroke-width="1px" d="M199 39q+160+20+160+160"/>
       """ +
       /* axis labels */
       """
       <text x="180" y="44" font-weight="400"><tspan x="180" y="44" font-size="16px">R</tspan></text>
-      <text x="106" y="190" font-weight="400"><tspan x="106" y="190" font-size="16px">R/2</tspan></text>
-      <text x="166" y="296" font-weight="400"><tspan x="166" y="296" font-size="16px">R/2</tspan></text>
+      <text x="106" y="190" font-weight="400"><tspan x="20" y="190" font-size="16px">R</tspan></text>
+      <text x="166" y="296" font-weight="400"><tspan x="209" y="360" font-size="16px">R</tspan></text>
       <text x="360" y="190" font-weight="400"><tspan x="360" y="190" font-size="16px">R</tspan></text>
+      <text x="360" y="190" font-weight="400"><tspan x="209" y="125" font-size="16px">R/2</tspan></text>
       <g id="graph__points">
       """ +
       renderPoints() +
