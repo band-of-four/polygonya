e494d4d04d82b7a4c4b184c779029b0f8a5600ba
timlathy <tim.labushev@gmail.com>
2018-10-31 18:17:05 +0300

    serve the spa index page

--- src/main/scala/b4/config/AssetsConfiguration.scala
+++ src/main/scala/b4/config/AssetsConfiguration.scala
@@ -1,10 +1,18 @@
 package b4.config
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.{ResourceHandlerRegistry, WebMvcConfigurer}
+import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.web.servlet.config.annotation.{ResourceHandlerRegistry, ViewControllerRegistry, WebMvcConfigurer}
+import org.springframework.web.servlet.view.InternalResourceViewResolver
 
 @Configuration
 class AssetsConfiguration extends WebMvcConfigurer {
+  @Bean
+  def getViewResolver: InternalResourceViewResolver =
+    new InternalResourceViewResolver() { setSuffix(".html") }
+
+  override def addViewControllers(registry: ViewControllerRegistry): Unit =
+    registry.addViewController("/").setViewName("index")
+
   override def addResourceHandlers(registry: ResourceHandlerRegistry): Unit =
     registry.addResourceHandler("/assets/**").addResourceLocations("file:client/dist/")
 }
