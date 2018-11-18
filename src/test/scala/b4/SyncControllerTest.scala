5ef0d09fd82fcb1dab90c5667dda4efcb5e29447
johnny-keker <giomail.iv@gmail.com>
2018-11-18 21:31:41 +0300

    info and perform tests

--- src/test/scala/b4/SyncControllerTest.scala
+++ src/test/scala/b4/SyncControllerTest.scala
@@ -15,19 +15,22 @@ class SyncControllerTest extends SpringIntegrationTest {
   private lazy val existingUserJson =
     "{\"username\": \"" + existingUser.username + "\", \"password\": \"lilium\"}"
   private lazy val performJson = "{\"newDay\":1, \"relationshipDelta\":1, \"history\":[{\"x\":1, \"y\":1}, {\"x\":2, \"y\":2}]}"
-
-  "/sync/info" should "return error if user is not athorized" in {
-    get("/sync/info") should be (401, None)
-  }
-
-
+  private lazy val anotherUser: User =
+    userRepository.save(new User("salmon", BCrypt.hashpw("ofwisdom", BCrypt.gensalt(12))))
+  private lazy val anotherUserJson =
+    "{\"username\": \"" + anotherUser.username + "\", \"password\": \"ofwisdom\"}"
 
   "/sync/info" should "return an actual information about current user" in {
+    get("/sync/info") should be (401, None)
     post("/auth/login", existingUserJson)
     get("/sync/info") should be (200,Some("{\"day\":0,\"relationship\":0,\"relationshipDelta\":0}"))
+    post("/sync/perform", performJson) should be (200, None)
+    get("/sync/info") should be (200,Some("{\"day\":1,\"relationship\":1,\"relationshipDelta\":1}"))
   }
+
   "/sync/perform" should "add given values to database" in {
-    post("/auth/login", existingUserJson)
+    post("/sync/perform", performJson) should be (401, None)
+    post("/auth/login", anotherUserJson)
     post("/sync/perform", performJson) should be (200, None)
   }
 }
