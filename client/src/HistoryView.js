71a27a155103b4094bb2aec18ed557f03c7d3ae8
johnny-keker <giomail.iv@gmail.com>
2018-11-19 14:22:54 +0300

    back to reality button

--- client/src/HistoryView.js
+++ client/src/HistoryView.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { APP_UI_GAME } from './reducers/app.js';
 
 
 class History extends Component {
   render() {
     return ( 
-      <h1> Hellow! </h1>
+      <button type="button" className="button" onClick={this.props.dispatchReturn}>Вернуться в реальность</button> 
     );
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  dispatchReturn: () => dispatch({ type: APP_UI_GAME })
+});
 
-const HistoryView = connect(() => ({}), () => ({}))(History)
+const HistoryView = connect(() => ({}), mapDispatchToProps)(History)
 
 export default HistoryView;
