7eb0bed2826e141a49b1ade7674f7767017010b1
johnny-keker <giomail.iv@gmail.com>
2018-11-19 13:01:29 +0300

    more tests to tests god

--- src/test/scala/b4/SyncControllerTest.scala
+++ src/test/scala/b4/SyncControllerTest.scala
@@ -14,23 +14,23 @@ class SyncControllerTest extends SpringIntegrationTest {
     userRepository.save(new User("lucy", BCrypt.hashpw("lilium", BCrypt.gensalt(12))))
   private lazy val existingUserJson =
     "{\"username\": \"" + existingUser.username + "\", \"password\": \"lilium\"}"
-  private lazy val performJson = "{\"newDay\":1, \"relationshipDelta\":1, \"history\":[{\"x\":1, \"y\":1}, {\"x\":2, \"y\":2}]}"
-  private lazy val anotherUser: User =
-    userRepository.save(new User("salmon", BCrypt.hashpw("ofwisdom", BCrypt.gensalt(12))))
-  private lazy val anotherUserJson =
-    "{\"username\": \"" + anotherUser.username + "\", \"password\": \"ofwisdom\"}"
 
   "/sync/info" should "return an actual information about current user" in {
+    val performJson =
+      "{\"newDay\":1, \"relationshipDelta\":1, \"history\":[{\"x\":1, \"y\":1}, {\"x\":2, \"y\":2}]}"
     get("/sync/info") should be (401, None)
+    post("/sync/perform", performJson) should be (401, None)
     post("/auth/login", existingUserJson)
     get("/sync/info") should be (200,Some("{\"day\":0,\"relationship\":0,\"relationshipDelta\":0}"))
     post("/sync/perform", performJson) should be (200, None)
     get("/sync/info") should be (200,Some("{\"day\":1,\"relationship\":1,\"relationshipDelta\":1}"))
   }
 
-  "/sync/perform" should "add given values to database" in {
-    post("/sync/perform", performJson) should be (401, None)
-    post("/auth/login", anotherUserJson)
-    post("/sync/perform", performJson) should be (200, None)
+  "/sync/history" should "return actual information about current user history" in {
+    get("/sync/history") should be (401, None)
+    post("/auth/login", existingUserJson)
+    get("/sync/history") should be (200, None)
   }
+
+
 }
