bdad890b19bbe14e4124b3702d9f7f71751054d3
timlathy <tim.labushev@gmail.com>
2018-11-23 18:49:16 +0300

    update graph svg

--- client/src/GraphSVG.js
+++ client/src/GraphSVG.js
@@ -34,25 +34,30 @@ export default class GraphSVG extends Component {
     return (
       <svg className="graph" ref="svg" onClick={this.placePoint}
            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
-        <g id="graph__coordinate-plane">
-          {/* horizontal and vertical axes */}
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M0 199h400"/>
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M199 0v400"/>
-          {/* arrows */}
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M199 0l-3 7"/>
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M199 0l3 7"/>
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M400 199l-6-3"/>
-          <path fill="none" stroke="#000" strokeWidth="1px" d="M400 199l-6 3"/>
+        <g fill="#6d6d6d">
+          <path d="M0 199l80 1h320a1 1 90 0 0 0-2H80l-80 1z"/>
+          <path d="M198 0a12362 12362 0 0 0-1 200l1 120 1 80v-80-120a16253 16253 0 0 1 1-200 1 1 90 0 0-2 0z"/>
+          <path d="M199 0l-1 1-1 2-1 2v2l1-1 1-2 1-2V0z"/>
+          <path d="M199 0v2l1 2 1 2 1 1V5l-1-2-1-2-1-1z"/>
+          <path d="M400 199l-1-1-2-1-2-1h-1l1 1 2 1 2 1h1z"/>
+          <path d="M400 199h-1l-2 1-2 1-1 1h1l2-1 2-1 1-1z"/>
         </g>
-        <path fill="none" stroke="#000" strokeWidth="1px" d="M39 199l160 160"/>
-        <path fill="none" stroke="#000" strokeWidth="1px" d="M39 119v80"/>
-        <path fill="none" stroke="#000" strokeWidth="1px" d="M39 119H199"/>
-        <path fill="none" stroke="#000" strokeWidth="1px" d="M199 39q+160+20+160+160"/>
-        <text x="180" y="44" fontWeight="400"><tspan x="180" y="44" fontSize="16px">R</tspan></text>
-        <text x="106" y="190" fontWeight="400"><tspan x="20" y="190" fontSize="16px">R</tspan></text>
-        <text x="166" y="296" fontWeight="400"><tspan x="209" y="360" fontSize="16px">R</tspan></text>
-        <text x="360" y="190" fontWeight="400"><tspan x="360" y="190" fontSize="16px">R</tspan></text>
-        <text x="360" y="190" fontWeight="400"><tspan x="209" y="125" fontSize="16px">R/2</tspan></text>
+        <path fill="#6d6d6d" d="M198 120a693 693 0 0 1 45 42l21 23 15 14-13-16-21-22a662 662 0 0 0-45-43 1 1 90 0 0-2 2z"/>
+        <path fill="#6d6d6d" d="M39 119l-1 18a700 700 0 0 0 0 62 1 1 90 0 0 2 0v-16-24-22l-1-18z"/>
+        <path fill="#6d6d6d" d="M39 119l32 1 48-1a3868 3868 0 0 1 80 1l1-1-1-1a2942 2942 0 0 0-80-1l-48 1-32 1z"/>
+        <path fill="#6d6d6d" d="M199 359c-7-1-19-1-33-4A151 151 0 0 1 49 249a167 167 0 0 1-9-50 1 1 90 0 0-2-1 140 140 0 0 0 9 52 176 176 0 0 0 41 67 144 144 0 0 0 78 39l33 3z"/>
+        <text x="100" y="190" fill="#6d6d6d">
+          <tspan x="14" y="190" fontFamily="Neucha" fontSize="17" fontWeight="400">-R</tspan>
+        </text>
+        <text x="166" y="296" fill="#6d6d6d">
+          <tspan x="209" y="360" fontFamily="Neucha" fontSize="17" fontWeight="400">R</tspan>
+        </text>
+        <text x="360" y="182" fill="#6d6d6d">
+          <tspan x="209" y="117" fontFamily="Neucha" fontSize="17" fontWeight="400">R/2</tspan>
+        </text>
+        <text x="418" y="282" fill="#6d6d6d">
+          <tspan x="267" y="217" fontFamily="Neucha" fontSize="17" fontWeight="400">R/2</tspan>
+        </text>
         <g id="graph__points">{this.renderPoints()}</g>
       </svg>
     );
